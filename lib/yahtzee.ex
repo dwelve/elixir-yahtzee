2b3c7a616f396e0f494c2fbab04809d7727b4487
Lue Andrie-Her <lue.her@gmail.com>
2015-05-13 23:42:59 -0500

    Fixed score_straight function. Game loop mostly finished. Added "chance" score category.

--- lib/yahtzee.ex
+++ lib/yahtzee.ex
@@ -4,7 +4,8 @@ defmodule Yahtzee do
 
   defstruct score: [ones: nil, twos: nil, threes: nil, fours: nil, fives: nil, sixes: nil,
                     three_of_kind: nil, four_of_kind: nil, full_house: nil,
-                    small_straight: nil, large_straight: nil, yahtzee: nil, bonus: 0]
+                    small_straight: nil, large_straight: nil, yahtzee: nil,
+                    chance: nil, upper_bonus: 0, yahtzee_bonus: 0]
 
   def score_functions, do: [
     ones: &(score_top_n(&1, 1)),
@@ -13,11 +14,18 @@ defmodule Yahtzee do
     fours: &(score_top_n(&1, 4)),
     fives: &(score_top_n(&1, 5)),
     sixes: &(score_top_n(&1, 6)),
-    three_of_kind: &score_three_of_kind/1, four_of_kind: &score_four_of_kind/1,
-    full_house: &score_full_house/1, small_straight: &score_small_straight/1,
-    large_straight: &score_large_straight/1, yahtzee: &score_yahtzee/1]
+    three_of_kind: &score_three_of_kind/1,
+    four_of_kind: &score_four_of_kind/1,
+    full_house: &score_full_house/1,
+    small_straight: &score_small_straight/1,
+    large_straight: &score_large_straight/1,
+    yahtzee: &score_yahtzee/1,
+    chance: &score_chance/1
+  ]
 
   def max_scores do
+    # todo:: get rid of everything but full_house, straights, and yahtzee... 
+    # and change name to fixed_scores...
     [ ones: 1*@num_dice,
       twos: 2*@num_dice,
       threes: 3*@num_dice,
@@ -29,7 +37,9 @@ defmodule Yahtzee do
       full_house: 25,
       small_straight: 30,
       large_straight: 40,
-      yahtzee: 50 ] 
+      yahtzee: 50,
+      chance: 6*@num_dice
+    ] 
   end
 
   def empty_dice, do: Stream.cycle([0]) |> Enum.take(@num_dice)
@@ -41,23 +51,61 @@ defmodule Yahtzee do
 
   def game_loop(dice, rolls_left, game=%Yahtzee{}) do
     print_game(dice, rolls_left, game)
-    hand_score = score(dice)
-    case rolls_left do
-      3 -> game_loop(roll(), 2, game)
-      2 -> print_hand_score(hand_score)
-           game_loop(ask_which(dice), 1, game)
-      1 -> print_hand_score(hand_score)
-           game_loop(ask_which(dice), 0, game)
-      0 -> print_hand_score(hand_score)
-           dice
+    if check_end_of_game(game) do
+      IO.puts "End of game!"
+    else
+      hand_score = score(dice, game)
+      case rolls_left do
+        3 -> game_loop(roll(), 2, game)
+        2 -> print_hand_score(hand_score)
+             game_loop(ask_which(dice), 1, game)
+        1 -> print_hand_score(hand_score)
+             game_loop(ask_which(dice), 0, game)
+        0 -> print_hand_score(hand_score)
+             game = pick_score(hand_score, game)
+             game_loop(empty_dice, 3, game)
+      end
     end
   end
+
+  def negate(x), do: !x
+  def check_end_of_game(game = %Yahtzee{}) do
+    # if nil is present as score value, that scoring option has not been played yet
+    Map.get(game, :score) |> Keyword.values |> Enum.any?(&(&1 == nil)) |> negate
+  end
+
+  def pick_score(hand_score, game) do
+    game_score = Map.get(game, :score)
+    category = IO.gets "Pick a scoring category to play: "
+    category = String.strip(category) |> String.to_atom
+
+    if !Keyword.has_key?(game_score, category) do
+      IO.puts "Invalid category: #{category}"
+      pick_score(hand_score, game)
+    else
+      if Keyword.get(game_score, category) == nil do
+        new_score = Keyword.get(hand_score, category)
+        updated_game_score = Keyword.update!(game_score, category, fn _ -> new_score end)
+        %{game | score: updated_game_score}
+      else
+        IO.puts("Category #{category} already played")
+        pick_score(hand_score, game)
+      end
+    end
+  end
+  
 
   def print_game(dice, rolls_left, game=%Yahtzee{}) do
     IO.puts "\n == Yahtzee Game == "
     IO.puts inspect(game)
+    IO.puts "Total score: #{get_total_score(game)}" 
     IO.puts "Rolls left: #{rolls_left}"
     IO.puts "Dice: #{inspect dice}"
+  end
+
+  def get_total_score(game = %Yahtzee{}) do
+    game_score = Map.get(game, :score)
+    game_score |> Enum.reduce(0, fn {_key, val}, acc -> if val != nil, do: acc+val, else: acc end)
   end
 
   def print_hand_score(s) do
@@ -97,11 +145,11 @@ defmodule Yahtzee do
 
   def score(dice, game = %Yahtzee{}) do
     scores = score(dice)
-    if (scores[:yahtzee] != 0) and (game[:bonus] != 0) do
-      game = Dict.put(game, :bonus, game[:bonus] + 100)
-    end
+    # insert special rules for yahtzee
     scores
   end
+
+  def score_chance(dice), do: Enum.sum(dice)
 
   def score_top_n(dice, n), do: Enum.count(dice, &(&1 == n)) * n
 
@@ -128,14 +176,16 @@ defmodule Yahtzee do
       end)
   end
 
-  def score_small_straight(dice), do: (if score_straight(dice, 4), do: max_scores[:small_straight], else: 0)
-  def score_large_straight(dice), do: (if score_straight(dice, 5), do: max_scores[:large_straight], else: 0)
+  def score_small_straight(dice), do: (if score_straight(dice, small_straights), do: max_scores[:small_straight], else: 0)
+  def score_large_straight(dice), do: (if score_straight(dice, large_straights), do: max_scores[:large_straight], else: 0)
 
-  def score_straight(dice, length) do
-    # normalize dice values to start at 1
-    min = Enum.min(dice)
-    dice2 = dice |> Enum.map(&(&1 - min + 1))
-    1 .. length |> Enum.map(&(&1 in dice2)) |> Enum.all?
+  def small_straights, do: [[1,2,3,4],[2,3,4,5],[3,4,5,6]]
+  def large_straights, do: [[1,2,3,4,5], [2,3,4,5,6]] 
+  def score_straight(dice, straights) do
+    checks = for ss <- straights do
+      Enum.map(ss, &(&1 in dice)) |> Enum.all?
+    end
+    Enum.any?(checks)
   end
 
 end
